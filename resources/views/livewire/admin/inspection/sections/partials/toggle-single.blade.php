b914f720d069802432f3feaf01e9b4f4244435d5
IQBAL-KAMBOH <theiqbal111@gmail.com>
2025-07-31 16:20:39 +0500

    Inspection form wizard

--- resources/views/livewire/admin/inspection/sections/partials/toggle-single.blade.php
+++ resources/views/livewire/admin/inspection/sections/partials/toggle-single.blade.php
@@ -1,9 +1,9 @@
 <div class="form-item">
     <label class="form-item-label">{{ $label }}</label>
-    <div class="btn-group btn-group-toggle d-flex" data-toggle="buttons">
+    <div class="btn-group btn-group-toggle d-flex flex-wrap" data-toggle="buttons">
         @foreach($options as $option)
         <button type="button" wire:click="setSingleSelection('{{ $property }}', '{{ $option }}')"
-            class="btn w-100
+            class="btn
                 @if(($reportData[$property] ?? null) == $option)
                     {{ in_array($option, ['Yes', 'Available', 'Safe', 'No visible fault', 'No Error', 'No smoke', '360 degree']) ? 'active-green' : 'active-red' }}
                 @else
