c704b91df37bebd4cf4f14325e61da699df28c86
kamran-kamboh <kambohkami330@gmail.com>
2025-10-13 15:37:19 -0700

    new setup

--- database/migrations/2025_07_24_102242_create_table_table_vehicle_models.php
+++ database/migrations/2025_07_24_102242_create_table_table_vehicle_models.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('vehicle_models', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('brand_id')->constrained()->onDelete('cascade');
+            $table->foreignId('brand_id');
             $table->string('name');
             $table->timestamps();
         });
