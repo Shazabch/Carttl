c743e51f35a55326cebe3aa2b82eee1b52c4a54b
mrip-dev <im950959@gmail.com>
2025-11-01 19:21:23 +0500

    Share able link test4

--- resources/views/admin/inspection/report-pdf-template-public.blade.php
+++ resources/views/admin/inspection/report-pdf-template-public.blade.php
@@ -761,7 +761,7 @@
         <i class="fa-solid fa-car-burst"></i> Damage Assessment Report
     </div>
     <div class="card-body">
-        @if($damages->count())
+        @if($reportInView->damages->count())
             <table class="details-table" style="width: 100%; border-collapse: collapse;">
                 <thead>
                     <tr style="background: #333; color: #fff;">
@@ -773,7 +773,7 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach($damages as $index => $damage)
+                    @foreach($reportInView->damages as $index => $damage)
                         @php
                             $typeInfo = $damageTypes[$damage->type] ?? ['name' => 'Unknown', 'color' => '#999'];
                             $badgeColor = match(strtolower($damage->severity)) {
