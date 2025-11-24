7afbd1bd7b5051305aa95af4cfaf8a42b7c55137
kamran-kamboh <kambohkami330@gmail.com>
2025-11-24 10:24:12 +0500

    user bookings

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Booking extends Model
 {
     protected $guarded = [];
+    public function vehicle()
+{
+    return $this->belongsTo(Vehicle::class, 'vehicle_id');
+}
+
 }
