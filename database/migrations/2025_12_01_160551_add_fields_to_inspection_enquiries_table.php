9276faf96fc9247b94efc7667b1319d733c3d6e8
kamran-kamboh <kambohkami330@gmail.com>
2025-12-01 17:09:40 +0500

    bit change

--- database/migrations/2025_12_01_160551_add_fields_to_inspection_enquiries_table.php
+++ database/migrations/2025_12_01_160551_add_fields_to_inspection_enquiries_table.php
@@ -1,24 +1,30 @@
 <?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::table('inspection_enquiries', function (Blueprint $table) {
-            $table->string('status')->nullable()->after('inspector_id');
-            $table->text('comment')->nullable()->after('status');
-            $table->text('comment_initial')->nullable()->after('comment');
-            $table->decimal('asking_price', 12, 2)->nullable()->after('comment_initial');
+             $table->decimal('offer_price', 12, 2)->nullable();
+            
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::table('inspection_enquiries', function (Blueprint $table) {
-            $table->dropColumn(['status', 'comment', 'comment_initial', 'asking_price']);
+            
+            $table->dropColumn('offer_price');
         });
     }
 };
